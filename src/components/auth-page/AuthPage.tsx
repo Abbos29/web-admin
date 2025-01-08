9d414cb7458f3e44c54447ca3f02e1796784ea39
Abbos29 <abbosvaliev2003@gmail.com>
2025-01-08 20:15:07 +0500

    feat: final v2

--- src/components/auth-page/AuthPage.tsx
+++ src/components/auth-page/AuthPage.tsx
@@ -18,6 +18,9 @@ const AuthPage: FC = () => {
       console.log(auth);
       setIsAuth(Boolean(auth.token));
       localStorage.setItem("access-token", auth.token);
+
+      // Релоад страницы после установки токена
+      window.location.reload();
     });
   }
 
@@ -59,11 +62,10 @@ const AuthPage: FC = () => {
         sx={{
           width: "40vw",
           height: {
-            xs: "clamp(50vh, 80vw, 70vh)", 
-            sm: "clamp(60vh, 75vw, 80vh)", 
-            md: "70vh",                    
+            xs: "clamp(50vh, 80vw, 70vh)",
+            sm: "clamp(60vh, 75vw, 80vh)",
+            md: "70vh",
           },
-      
           margin: "10vh auto",
           minWidth: "320px",
           maxWidth: "700px",
@@ -84,15 +86,16 @@ const AuthPage: FC = () => {
             variant="h2"
             sx={{
               fontSize: {
-                xs: 'clamp(1.5rem, 5vw, 2.5rem)', 
-                sm: 'clamp(2rem, 4vw, 3rem)',     
-                md: 'clamp(2.5rem, 3vw, 3.5rem)', 
+                xs: "clamp(1.5rem, 5vw, 2.5rem)",
+                sm: "clamp(2rem, 4vw, 3rem)",
+                md: "clamp(2.5rem, 3vw, 3.5rem)",
               },
-              fontWeight: 700, 
-              textAlign: 'center', 
+              fontWeight: 700,
+              textAlign: "center",
             }}
-
-          >LfSafeSearch-Admin</Typography>
+          >
+            LfSafeSearch-Admin
+          </Typography>
           <TextField
             value={login}
             onChange={(e) => setLogin(e.target.value)}
@@ -105,6 +108,7 @@ const AuthPage: FC = () => {
             onChange={(e) => setPassword(e.target.value)}
             variant="filled"
             label="Пароль"
+            type="password"
             sx={{ width: "80%" }}
           ></TextField>
           <Button variant="contained" onClick={clickSubmit}>
@@ -115,7 +119,11 @@ const AuthPage: FC = () => {
           {/* Ссылка на страницу регистрации */}
           <Link
             href="/registration"
-            sx={{ marginTop: "16px", textAlign: "center", textDecoration: "none" }}
+            sx={{
+              marginTop: "16px",
+              textAlign: "center",
+              textDecoration: "none",
+            }}
           >
             Регистрация
           </Link>
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,40 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import AuthPage from "./components/auth-page/AuthPage";
 import AddPage from "./components/add-page/AddPage";
 import SneakersPage from "./components/sneakers-page/SneakersPage";
 import SneakerPage from "./components/sneaker-page/SneakerPage";
 import SignUpPage from "./components/sigup-page/SigupPage";
+
+
+const theme = createTheme({
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          padding: "14px 22px",
+          borderRadius: "12px",
+          fontSize: "14px",
+          fontWeight: 800,
+          color: "#fff",
+          backgroundColor: "#6a6c94",
+          boxShadow: "0 4px 4px 0 #0000001d",
+          border: "1px solid #858585",
+          textAlign: "center",
+          textTransform: "none",
+          "&:hover": {
+            backgroundColor: "#5a5b84", // Цвет при наведении
+          },
+        },
+      },
+    },
+  },
+});
+
+export default theme;
+
 
 const router = createBrowserRouter(
   [
@@ -25,10 +54,10 @@ const router = createBrowserRouter(
     },
     {
       path: "/add",
-      element: <AddPage />
+      element: <AddPage />,
     },
     {
-      path: "/edit/:id", 
+      path: "/edit/:id",
       element: <SneakerPage />,
     },
   ],
@@ -48,11 +77,13 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider
-      router={router}
-      future={{
-        v7_startTransition: true,
-      }}
-    />
+    <ThemeProvider theme={theme}>
+      <RouterProvider
+        router={router}
+        future={{
+          v7_startTransition: true,
+        }}
+      />
+    </ThemeProvider>
   </React.StrictMode>
 );
